17bd4bbe82a1fad43466dd8f2836d49883efb27f
KhanhNguyen <khanhnmngcd210430@fpt.edu.vn>
2023-02-09 15:00:32 +0700

    lab 9/2

---
+++ Lab9-2/Animals/Animal.cs
@@ -0,0 +1,40 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; protected set; }
+
+        public string FavoriteFood { get; protected set; }
+
+        protected Animal(string name, string favoriteFood)
+        {
+            this.Name = name;
+            this.FavoriteFood = favoriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favorite food is {this.FavoriteFood}";
+        }
+    }
+    public class Cat : Animal
+    {
+        public Cat(string name, string favoriteFood) : base(name, favoriteFood)
+        {
+        }
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + Environment.NewLine + "MEEOW";
+        }
+    }
+    public class Dog : Animal
+    {
+        public Dog(string name, string favoriteFood) : base(name, favoriteFood)
+        {
+        }
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + Environment.NewLine + "GRRRR";
+        }
+    }
+}
---
+++ Lab9-2/Shapes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Shapes
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Animal cat = new Cat("Pesho", "Cat food");
+            Animal dog = new Dog("Gosho", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+        }
+    }
+}
